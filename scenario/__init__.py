1345b5f8cf66be21ffc30fab3b68ba13ce029cdc
jshmilo <jshmilo@gmail.com>
2025-12-20 16:23:31 +0000

    Fix imports

--- scenario/__init__.py
+++ scenario/__init__.py
@@ -1,7 +1,8 @@
 __all__ = ['run_scenario', 'parse_scenario_json', 'play_scenario',
-           'get_timeout_feedback_json', 'get_overflow_feedback_json']
+           'get_timeout_feedback_json', 'get_overflow_feedback_json', 'run_unittest']
 
 from scenario.runner import run_scenario
 from scenario.parser import parse_scenario_json
 from scenario.player import play_scenario
 from scenario.api import get_timeout_feedback_json, get_overflow_feedback_json
+from scenario.unittest import run_unittest
