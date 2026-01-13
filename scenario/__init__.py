32c1b7c02c680a838fd9872f7371a30f32dc3bb5
jshmilo <jshmilo@gmail.com>
2026-01-13 16:38:44 +0000

    Added unittest to scenario -- v1.0

--- scenario/__init__.py
+++ scenario/__init__.py
@@ -1,8 +1,9 @@
 __all__ = ['run_scenario', 'parse_scenario_json', 'play_scenario',
-           'get_timeout_feedback_json', 'get_overflow_feedback_json', 'run_unittest']
+           'get_timeout_feedback_json', 'get_overflow_feedback_json', 'run_unittest', 'play_unittest']
 
 from scenario.runner import run_scenario
+from scenario.runner import run_unittest
 from scenario.parser import parse_scenario_json
 from scenario.player import play_scenario
 from scenario.api import get_timeout_feedback_json, get_overflow_feedback_json
-from scenario.unittest import run_unittest
+from scenario.unittest import play_unittest
